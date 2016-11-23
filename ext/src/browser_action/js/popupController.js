40db6968f398844745ac309a360bacefa6626439
Gareth Lyle <gareth_lyle@yahoo.com>
2016-11-23 13:06:43 +0200

    popupController update

--- ext/src/browser_action/js/popupController.js
+++ ext/src/browser_action/js/popupController.js
@@ -2,8 +2,18 @@ app.controller("popupController", function($scope){
 
 	
 	$scope.SliderChange = function(){
-		console.log($scope.rangeVal);
-	}
+		$scope.min = 0;
+		$scope.max = 2;
+
+			if($scope.rangeVal == $scope.min){
+				console.log('minimum filter');
+				} else if ($scope.rangeVal == $scope.max){
+					console.log('maximum filter');
+						} else {
+							console.log('medium filter');
+							}
+			}
+			//console.log($scope.rangeVal);
 	
 	
 })
