5c89b4e342961a23485e566156148bf5d2200303
Gareth Lyle <gareth_lyle@yahoo.com>
2016-11-24 15:03:32 +0200

    storage manifest and controller

--- ext/src/browser_action/js/popupController.js
+++ ext/src/browser_action/js/popupController.js
@@ -17,8 +17,23 @@ app.controller("popupController", function($scope){
 	            break;
 	        default:
 	    }
+
+	    // saves user selection.
+	    saveChanges();
 	}
 
 	$scope.SliderChange();	
-	
+
+	function saveChanges() {
+        // Get a value saved from input.
+        var theValue = $scope.rangeVal;
+        // Check that there's some code there.
+        if (!theValue) {
+          console.log($scope.rangeVal);
+          return;
+        }
+        // Save it using the Chrome extension storage API.
+        chrome.storage.sync.set({'HerbertFilterLevel': theValue});
+      }
+
 })
