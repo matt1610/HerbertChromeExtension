1b3646cd787247b1552e694680af8f7fd19bdea5
Matthew Starkey <mattstarkey@me.com>
2016-11-28 13:59:40 +0200

    Logic updates

--- ext/src/browser_action/js/popupController.js
+++ ext/src/browser_action/js/popupController.js
@@ -1,7 +1,11 @@
 app.controller("popupController", function($scope){
+
+	$scope.rangeVal = "0";
 	
-	$scope.rangeVal = chrome.storage.sync.get('HerbertFilterLevel', function(data){
+	chrome.storage.sync.get('HerbertFilterLevel', function(data){
 		$scope.rangeVal = data.HerbertFilterLevel;
+		$scope.$apply();
+		$scope.SliderChange();
 	});
 
 	$scope.SliderChange = function(){
